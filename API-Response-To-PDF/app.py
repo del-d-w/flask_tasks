4cf8e51994110e34aef97ce2b602bba301988b1b
MBhargavi235 <106804698+MBhargavi235@users.noreply.github.com>
2023-03-21 22:05:06 +0530

    Create app.py

---
+++ API-Response-To-PDF/app.py
@@ -0,0 +1,42 @@
+# pip install FPDF
+
+from fpdf import FPDF
+import requests
+
+url = 'https://jsonplaceholder.typicode.com/todos'
+
+# Make a request to the API
+response = requests.get(url)
+data = response.json()
+
+# Create a new PDF object
+pdf = FPDF()
+
+# Add a page
+pdf.add_page()
+
+# Set the font and font size
+pdf.set_font("Arial", size=12)
+
+# Set the header row
+pdf.cell(20, 10, "User ID", border=1)
+pdf.cell(15, 10, "ID", border=1)
+pdf.cell(135, 10, "Title", border=1)
+pdf.cell(25, 10, "Status", border=1)
+pdf.ln()
+
+# Add the data rows
+# here 20 is width of the cell and 10 is height of the cell
+"""
+pdf.ln() is a method of the FPDF class in the fpdf library, used to move the current position to the next line.
+It is used to add a new line after each row of data when generating a PDF document.
+"""
+for row in data:
+    pdf.cell(20, 10, str(row["userId"]), border=1)
+    pdf.cell(15, 10, str(row["id"]), border=1)
+    pdf.cell(135, 10, row["title"], border=1)
+    pdf.cell(25, 10,  str(row["completed"]), border=1)
+    pdf.ln()
+
+# Save the PDF
+pdf.output("output.pdf")
