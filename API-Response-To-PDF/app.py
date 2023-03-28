4f122cec77233a8f0db7cd4138e8dcce5a69094e
MBhargavi235 <106804698+MBhargavi235@users.noreply.github.com>
2023-03-28 19:37:08 +0530

    Update app.py

--- API-Response-To-PDF/app.py
+++ API-Response-To-PDF/app.py
@@ -49,7 +49,7 @@ pdf.output("output.pdf")
 
 
 
-Add background color
+# Add background color for entire page
 
 
 
@@ -90,6 +90,51 @@ pdf.ln()
 pdf.ln() is a method of the FPDF class in the fpdf library, used to move the current position to the next line.
 It is used to add a new line after each row of data when generating a PDF document.
 """
+for row in data:
+    pdf.cell(20, 10, str(row["userId"]), border=1)
+    pdf.cell(15, 10, str(row["id"]), border=1)
+    pdf.cell(135, 10, row["title"], border=1)
+    pdf.cell(25, 10,  str(row["completed"]), border=1)
+    pdf.ln()
+
+# Save the PDF
+pdf.output("output.pdf")
+
+-----------------------------------------------------------------------------------------------------------------------
+
+# background color for header row
+
+from fpdf import FPDF
+import requests
+
+url = 'https://jsonplaceholder.typicode.com/todos'
+
+# Make a request to the API
+response = requests.get(url)
+data = response.json()
+
+# Create a new PDF object
+pdf = FPDF()
+
+# Add a page
+pdf.add_page()
+
+# Set the font and font size
+pdf.set_font("Arial", size=12)
+
+# Set the background color to white
+pdf.set_fill_color(255, 255, 255)
+
+# Set the header row
+pdf.set_fill_color(173, 216, 230)  # Set background color for header row
+pdf.cell(20, 10, "User ID", border=1, fill=True)
+pdf.cell(15, 10, "ID", border=1, fill=True)
+pdf.cell(135, 10, "Title", border=1, fill=True)
+pdf.cell(25, 10, "Status", border=1, fill=True)
+pdf.ln()
+pdf.set_fill_color(0, 255, 255)  # Set background color back to white
+
+# Add the data rows
 for row in data:
     pdf.cell(20, 10, str(row["userId"]), border=1)
     pdf.cell(15, 10, str(row["id"]), border=1)
