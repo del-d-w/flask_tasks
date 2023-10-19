77ce58b31475469b028808595c55f082a9e1bb97
Bhargavi Mopuru <106804698+MBhargavi235@users.noreply.github.com>
2023-10-19 19:49:51 +0530

    Add files via upload

---
+++ hoisting/hoisting.js
@@ -0,0 +1,147 @@
+// demo()
+// function demo()
+// {
+// console.log("Hello ")
+// }
+// demo1(name);
+//  var name="prathyusha";
+//  function demo1(name)
+//  {
+//     var id=1;
+//     username=name;
+//     console.log(id,username);
+//  }
+
+//  let a=null;
+//  console.log(a);
+//  var b=undefined;
+//  if(a==undefined)
+//  {
+//    console.log("null and undefined  both are equal")
+//  }
+//  else{
+//    console.log("Null and undefined  both are not equal")
+//  }
+ 
+// function a()
+// {
+//    b()
+//    console.log("Functon a")
+// }
+// function b()
+// {
+// console.log("Function b")
+// }
+// a()
+// let d=20;
+// function a()
+// {
+// b()
+// var c=10;
+// console.log("Function a is"+c)
+// }
+// function b()
+// {
+// console.log("function b is"+d);
+// }
+// a();
+//Arithmetic operators
+
+// let a=10;
+// let b=20;
+// function demo()
+// {
+//     let c=a+b;
+//     let d=a-b;
+//     let e=a*b;
+//     console.log("addition:"+c+" "+"substraction:"+d+" "+"multiplication:"+e);
+// }
+// demo();
+// logical operators
+// function  demo1()
+// {
+//     var a=10;
+//     var b=20;
+//     if(a>=5 && b<=30)
+//     {
+//         console.log("Hello");
+//     }
+//     else{
+//         console.log("Hii");
+//     }
+// }
+// demo1();
+//Assignment Operator
+// let a=10;
+// let b=20;
+// function demo2()
+// {
+// if(a==b)
+// {
+//     console.log("a and b both are equal");
+// }
+// else{
+//     console.log("a and b are not equal");
+// }
+
+// }
+// demo2();
+//Coercion
+// var x=10+'10';
+// var y=10-'10';
+// console.log(x);
+// console.log(y);
+
+// var a=true+10;
+// var b=false+20;
+// console.log(a);
+// console.log(b);
+// console.log(10<5);
+// console.log(false==0);
+// console.log(""==0);
+
+// var a=0;
+// var b=false;
+// if(a===b)
+// {
+//     console.log('They are equal!');
+// }
+// else{
+//     console.log('Nope , not equal');
+// }
+
+// function greet(name)
+// {
+//     console.log('hello'+name);
+// }
+// greet();
+
+// var person=new Object();
+// person["firstname"]="Prathyusha";
+// person["lastname"]="Panta";
+
+// var firstNameProperty="firstname";
+// console.log(person);
+// console.log(person[firstNameProperty]);
+
+// person.address=new Object();
+// person.address.city="Giddalur";
+// person.address.state="AP";
+
+// console.log(person.address);
+
+
+// var  demo=function()
+// {
+//     console.log("Hii JavaScript");
+// }
+// demo();
+function log(a)
+{
+    console.log(a)
+
+}
+log(function()
+{
+console.log("hii");
+})
